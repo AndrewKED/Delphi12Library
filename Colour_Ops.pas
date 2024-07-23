10035477f494936a2cb7e8bcf090826e28131c37
AndrewSpencerTraX <AndrewSpencer@Andrew-TBDEV.TRAX.local>
2024-07-23 12:22:32 +0200

    Colour_Ops: Added constants for additional colours and colour sets. Added CanChangeColour function to check whether proposed shade change will work

--- Colour_Ops.pas
+++ Colour_Ops.pas
@@ -7,6 +7,8 @@ uses
 
 function ChangeColourShade(colourOriginal : TColor;
                            iChange : Longint) : Integer;
+function CanChangeColour(colourOriginal : TColor;
+                         proposedChange : Longint) : Boolean;
 function ShadeBetween(StartColour : TColor;
                       EndColour : TColor;
                       Percentage : double) : TColor;
@@ -21,10 +23,12 @@ const
   clLtSilver        = $00E0E0E0;
 //clSilver          = $00C0C0C0
 //clGray            = $00808080
-  clTextOnWhite     = $00303030;    // Non-black, for white background, to reduce eye strain
+  clTextOnWhite     = $00404040;    // Non-black, for white background, to reduce eye strain
 
   clLtLtRed         = $00CCCCFF;
   clHighlightPink   = $00FF80FF;
+
+  clLtRed           = $00C0C0FF;
 
   clLtLtOrange      = $00B0D0FF;
   clLightOrange     = $0080C0FF;
@@ -50,6 +54,79 @@ const
 
   clDocBackground   = $00FFF0D6;    // A Light blue
 
+  // 6 Light blue-green colours
+  // ChatGPS question : "Provide 6 random, distinct colours with a light blue/green theme. The colours should be in hexadecimal Pascal format BGR, with a leading zero byte"
+  csBlueGreen6 : array[0..5] of TColor = (
+//    $00ADD8E6, // (Light Blue)
+//    $009ACD32, // (Yellow-Green)
+//    $0070DB93, // (Turquoise)
+//    $0056FFA4, // (Mint Green)
+//    $00B0E0E6, // (Powder Blue)
+//    $0080FF00 // (Lime Green)
+
+    $00ADD8E6, // (Light Blue)
+    $0000FA9A, // (Medium Spring Green)
+    $0020B2AA, // (Light Sea Green)
+    $000080FF, // (Light Sky Blue)
+    $009ACD32, // (Yellow-Green)
+    $002E8B57  // (Sea Green)
+
+  );
+
+  // This colour set of random, distinct blue/green shades was produced by ChatGPT
+  // From further experiene with Chat GPT, there is no guarantee that these are "distinct"!
+  csBlueGreen50 : array[0..49] of TColor = (
+    $00FF8000,
+    $0032CD32,
+    $004682B4,
+    $001E90FF,
+    $003CB371,
+    $0000BFFF,
+    $0066CDAA,
+    $0000CED1,
+    $0000FF7F,
+    $005A8268,
+    $004F4F2F,
+    $0040E0D0,
+    $00ADD8E6,
+    $00008B8B,
+    $0020B2AA,
+    $00008080,
+    $003CB371,
+    $0020B2AA,
+    $0000FA9A,
+    $009E9E5F,
+    $0000FF00,
+    $0020B2AA,
+    $0066CDAA,
+    $003CB371,
+    $007FFFD4,
+    $0032CD32,
+    $00008B8B,
+    $0087CEFA,
+    $004682B4,
+    $00008080,
+    $0032CD99,
+    $005A8268,
+    $0020B2AA,
+    $0000FF7F,
+    $0000BFFF,
+    $000080FF,
+    $002E8B57,
+    $0087CEEB,
+    $0000FA9A,
+    $0000FF00,
+    $00008B8B,
+    $0066CDAA,
+    $00008080,
+    $004682B4,
+    $0020B2AA,
+    $0000BFFF,
+    $0032CD32,
+    $009E9E9F,
+    $007FFFD4,
+    $00D4FF7F);
+
 implementation
 
 uses
@@ -59,18 +136,25 @@ uses
 //
 //  FUNCTION  : ChangeColourShade
 //
-//  I/P       :
+//  I/P       : colourOriginal : TColor - The colour to be changed
 //
-//  O/P       :
+//              iChange : Integer - the amount by which each of the RGB
+//                element values is to be changed.
+//
+//  O/P       : Longint - the new colour
+{TODO -oAndrew Spencer -cCheck : Why did I use Longint and not TColor?}
 //
 //  OPERATION : Each colour byte is adjusted up or down by the given change.
-//              Will not work for system colours (e.g. clHighlight)
+//
+//              Note that this does not work for system colours (e.g. clHighlight)
+//              One would have to use TStyleManager.ActiveStyle.GetSystemColor()
+//              to determine the actual RGB
 //
 //  UPDATED   : 2017-07-21
 //
 //***************************************************************************
 function ChangeColourShade(colourOriginal : TColor;
-                           iChange : integer) : Longint;
+                           iChange : Integer) : Longint;
 var
   iBlue : Integer;
   iRed : Integer;
@@ -89,6 +173,49 @@ begin
 
   result := (iBlue shl 16) + (iGreen shl 8) + iRed;
 end;
+
+//***************************************************************************
+//
+//  FUNCTION  : CanChangeColour
+//
+//  I/P       :
+//
+//  O/P       :
+//
+//  OPERATION : Test whether the given change to RGB will make an appreciable
+//              change to the colour.
+//
+//              "appreciable change" is where any one of the colours is able
+//              to be changed by the proposed colour change.
+//
+//  UPDATED   : 2024-07-22
+//
+//***************************************************************************
+function CanChangeColour(colourOriginal : TColor;
+                         proposedChange : Longint) : Boolean;
+var
+  blue : Integer;
+  red : Integer;
+  green : Integer;
+  blueNew : Integer;
+  redNew : Integer;
+  greenNew : Integer;
+
+begin
+  colourOriginal := Graphics.ColorToRGB(colourOriginal);
+
+  blue := (colourOriginal and $00FF0000) shr 16;
+  green := (colourOriginal and $0000FF00) shr 8;
+  red := (colourOriginal and $000000FF);
+
+  blueNew := EnsureRange(blue + proposedChange, $00, $FF);
+  greenNew := EnsureRange(green + proposedChange, $00, $FF);
+  redNew := EnsureRange(red + proposedChange, $00, $FF);
+
+  result := (blueNew - blue = proposedChange) or
+            (greenNew - green = proposedChange) or
+            (redNew - red = proposedChange);
+end; // CanChangeColour
 
 //***************************************************************************
 //
