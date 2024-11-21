6756bfaf2f15be18561e893605c6d6caa84a2624
AndrewSpencerTraX <AndrewSpencer@Andrew-TBDEV.TRAX.local>
2024-11-21 16:52:27 +0200

    uniGUI_Ops: Added ResizeGridColumns to fit the columns of a TUniDBGrid ratiometrically into the available width

--- uniGUI_Ops.pas
+++ uniGUI_Ops.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   System.SysUtils,
-  uniGUIClasses, uniGUIForm, uniPanel;
+  uniGUIClasses, uniGUIForm, uniPanel, uniDBGrid;
 
 const
   PERSISTENT_COOKIE_EXPIRY = 73051; // Equivalend of EncodeDate(2100,1,1)
@@ -26,12 +26,15 @@ procedure SetReadOnly(owner : TUniContainer;
                       handleButtons : Boolean);
 procedure SetDateFormat(owner : TUniContainer;
                         newFormat : String);
+procedure uniDBGridAutoWidth(aGrid: TUniDBGrid; aFieldName: String = '');
+procedure ResizeGridColumns(theGrid : TUniDBGrid;
+                            columnRatios : array of Integer); overload;
 
 implementation
 
 uses
-  System.TypInfo,
-  uniButton;
+  System.TypInfo, System.Classes,
+  uniButton, uniBasicGrid;
 //  uniCheckBox, uniEdit, uniDateTimePicker, uniMemo,
 //  uniDBCheckBox, uniDBEdit, uniDBDateTimePicker, uniDBMemo;
 
@@ -209,5 +212,160 @@ begin
     end;
   end; // for
 end;
+
+//***************************************************************************
+//
+//  FUNCTION  : UniDBGridAutoWidth
+//
+//  I/P       :
+//
+//  O/P       :
+//
+//  OPERATION :
+//
+//              From https://forums.unigui.com/index.php?/topic/4627-set-tunidbgrids-column-width-with-percentage/
+//
+//  UPDATED   : 2024-11-18
+//
+//***************************************************************************
+procedure uniDBGridAutoWidth(aGrid: TUniDBGrid; aFieldName: String = '');
+var
+  s: String;
+  i: Integer;
+
+begin
+  if aFieldName = '' then
+  begin
+    s := 'sender.headerCt.forceFit=true;'
+  end
+  else
+  begin
+    with TStringList.Create do
+    try
+      Delimiter := ';';
+      StrictDelimiter := True;
+      DelimitedText := LowerCase(aFieldName);
+      for i := 0 to Pred(aGrid.Columns.Count) do
+      begin
+        if IndexOf(LowerCase(aGrid.Columns[i].FieldName)) <> -1 then
+        begin
+          s := s + Format('columns[%d].flex=1;', [i]);
+        end;
+      end;
+    finally
+      Free;
+    end;
+  end; // else
+
+  if s <> '' then
+  begin
+    aGrid.ClientEvents.ExtEvents.Add(Format(
+      'beforereconfigure=function beforereconfigure(sender, store, columns, oldStore, the, eOpts)' +
+      '{%s}', [s])
+    );
+  end;
+end;
+
+//***************************************************************************
+//
+//  FUNCTION  : GetGridClientWidth
+//
+//  I/P       :
+//
+//  O/P       :
+//
+//  OPERATION : Returns the amount of horizontal space that could be
+//              apportioned between cells in a grid
+//
+//  UPDATED   :
+//
+//***************************************************************************
+function GetGridClientWidth(cgTarget : TUniBasicGrid) : Integer;
+begin
+  Result := cgTarget.Width - 20; // GetSystemMetrics(SM_CXVSCROLL);
+  // Only available in TDBGrids
+//  if (cgTarget is TDBGrid) then
+//  begin
+//    if (dgIndicator in (cgTarget as TDBGrid).Options) then
+//    begin
+//      result := result - INDICATORWIDTH;
+//      if dgColLines in (cgTarget as TDBGrid).Options then
+//        Dec(result);
+//    end; // if
+//    if (dgColLines in (cgTarget as TDBGrid).Options) then
+//      result := result - (cgTarget as TDBGrid).Columns.Count;
+//  end; // if
+//
+//  if (((cgTarget is TDBGrid) and
+//       ((cgTarget as TDBGrid).Ctl3D)) or
+//      ((cgTarget is TStringGrid) and
+//       ((cgTarget as TStringGrid).Ctl3D))) then
+//  begin
+//    if (((cgTarget is TDBGrid) and
+//         ((cgTarget as TDBGrid).BorderStyle = bsSingle)) or
+//        ((cgTarget is TStringGrid) and
+//         ((cgTarget as TStringGrid).BorderStyle = bsSingle))) then
+//      // 2 * 2 Pixel
+//      result := result - 4
+//    else
+//      // 2 * 1 Pixel
+//      result := result - 2;
+//  end; // if
+//
+//  if ((cgTarget is TStringGrid) and
+//      (goVertLine in (cgTarget as TStringGrid).Options)) then
+//    result := result -
+//              (cgTarget as TStringGrid).ColCount * (cgTarget as TStringGrid).GridLineWidth;
+//
+//
+end; // GetGridClientWidth
+
+//***************************************************************************
+//
+//  FUNCTION  :
+//
+//  I/P       :
+//
+//  O/P       :
+//
+//  OPERATION :
+//
+//  UPDATED   :
+//
+//***************************************************************************
+procedure ResizeGridColumns(theGrid : TUniDBGrid;
+                            columnRatios : array of Integer); overload;
+var
+  iClientWidth : Integer;
+  t : Integer;
+  iTotal : Integer;
+
+begin
+  if (Length(columnRatios) = theGrid.Columns.Count) then
+  begin
+    iTotal := 0;
+    for t := 0 to theGrid.Columns.Count-1 do
+    begin
+      if (theGrid.Columns[t].Visible) then
+      begin
+        iTotal := iTotal + columnRatios[t];
+      end;
+    end; // for
+
+    iClientWidth := GetGridClientWidth(theGrid);
+
+    if (iTotal <> 0) then
+    begin
+      for t := 0 to theGrid.Columns.Count-1 do
+      begin
+        if (theGrid.Columns[t].Visible) then
+        begin
+          theGrid.Columns[t].Width := iClientWidth * columnRatios[t] div iTotal;
+        end;
+      end; // for
+    end; // if
+  end;
+end; // ResizeGridColumns
+
 
 end.
