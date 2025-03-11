ef64172d8bbcce7ac0b8cb62e8e68610bb90adb5
AndrewSpencerTraX <AndrewSpencer@Andrew-TBDEV.TRAX.local>
2025-03-11 15:20:38 +0200

    SQL_Ops: Added. Includes some useful constants

---
+++ SQL_Ops.pas
@@ -0,0 +1,18 @@
+unit SQL_Ops;
+
+interface
+
+const
+  // Just useful reminders of how to test(!)
+  // (Keep the leading space in place)
+  // https://www.w3schools.com/sql/sql_null_values.asp
+  IS_NULL = ' IS NULL';
+  IS_NOT_NULL = ' IS NOT NULL';
+
+  MYSQL_DATETIME_FORMAT = 'yyyy-mm-dd hh:nn:ss.zzz';
+  MYSQL_YMDHN_FORMAT = 'yyyy-mm-dd hh:nn';
+  MYSQL_DATE_FORMAT = 'yyyy-mm-dd';
+
+implementation
+
+end.
